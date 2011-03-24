5383a66f32d1f51d855201884b25cef11575c87d
knut funkel <knutfunkel@knut-funkels-MacBook.local>
2011-03-24 13:59:43 +0100

    added log page and fixed gtdapp login

---
+++ Resources/gtd.js
@@ -0,0 +1,37 @@
+//
+// create base UI tab and root window
+//
+var win1 = Titanium.UI.createWindow({  
+    title:'Welcome',
+    backgroundColor:'#fff'
+});
+var tab1 = Titanium.UI.createTab({  
+    icon:'KS_nav_views.png',
+    title:'Welcome to the GTD viewer',
+    window:win1
+});
+
+var label1 = Titanium.UI.createLabel({
+	color:'#999',
+	text:'Welcome to the GTD viewer',
+	font:{fontSize:20,fontFamily:'Helvetica Neue'},
+	textAlign:'center',
+	width:'auto'
+});
+
+win1.add(label1);
+
+Ti.include(
+  'login.js'
+);
+
+Ti.include(
+  'log.js'
+);
+
+//
+//  add tabs
+//
+tabGroup.addTab(tab1);  
+tabGroup.addTab(tab2); 
+tabGroup.addTab(tab3); 
