56aab70b3d777af0d09b1bfcaac2edd37d31f1e6
Knut Funkel <knut.funkel@gmail.com>
2011-05-15 15:34:43 +0200

    added scroll down for refresh

--- Resources/Views/tasks.js
+++ Resources/Views/tasks.js
@@ -6,11 +6,171 @@ var pass = Titanium.App.Properties.getString("pass");
 
 Ti.include('../net/getTasks.js', '../utils/taskParsers.js', '../Editors/task_editor.js', '../utils/quadrant.js', '../utils/taskSorter.js', '../database/tasksDB.js');
 
-createNewTableView = function(tableData){
-    var tableview = Titanium.UI.createTableView();
+createNewTableView = function(){
+    var tableView = Titanium.UI.createTableView();
     
-    tableview.addEventListener('click', function(e){
-        Titanium.API.info("tableview event triggered: " + e.rowData.title);
+    function formatDate(){
+        var date = new Date();
+        var datestr = date.getMonth() + '/' + date.getDate() + '/' + date.getFullYear();
+        if (date.getHours() >= 12) {
+            datestr += ' ' + (date.getHours() == 12 ? date.getHours() : date.getHours() - 12) + ':' + date.getMinutes() + ' PM';
+        }
+        else {
+            datestr += ' ' + date.getHours() + ':' + date.getMinutes() + ' AM';
+        }
+        return datestr;
+    }
+    
+    var border = Ti.UI.createView({
+        backgroundColor: "#576c89",
+        height: 2,
+        bottom: 0
+    });
+    
+    var tableHeader = Ti.UI.createView({
+        backgroundColor: "#e2e7ed",
+        width: 320,
+        height: 60
+    });
+    
+    // fake it til ya make it..  create a 2 pixel
+    // bottom border
+    tableHeader.add(border);
+    
+    var arrow = Ti.UI.createView({
+        backgroundImage: "../images/whiteArrow.png",
+        width: 23,
+        height: 60,
+        bottom: 10,
+        left: 20
+    });
+    
+    var statusLabel = Ti.UI.createLabel({
+        text: "Pull to reload",
+        left: 55,
+        width: 200,
+        bottom: 30,
+        height: "auto",
+        color: "#576c89",
+        textAlign: "center",
+        font: {
+            fontSize: 13,
+            fontWeight: "bold"
+        },
+        shadowColor: "#999",
+        shadowOffset: {
+            x: 0,
+            y: 1
+        }
+    });
+    
+    var lastUpdatedLabel = Ti.UI.createLabel({
+        text: "Last Updated: " + formatDate(),
+        left: 55,
+        width: 200,
+        bottom: 15,
+        height: "auto",
+        color: "#576c89",
+        textAlign: "center",
+        font: {
+            fontSize: 12
+        },
+        shadowColor: "#999",
+        shadowOffset: {
+            x: 0,
+            y: 1
+        }
+    });
+    
+    var actInd = Titanium.UI.createActivityIndicator({
+        left: 20,
+        bottom: 13,
+        width: 30,
+        height: 30
+    });
+    
+    tableHeader.add(arrow);
+    tableHeader.add(statusLabel);
+    tableHeader.add(lastUpdatedLabel);
+    tableHeader.add(actInd);
+    
+    tableView.headerPullView = tableHeader;
+    
+    
+    var pulling = false;
+    var reloading = false;
+    
+    function beginReloading(){
+        // just mock out the reload
+        setTimeout(endReloading, 2000);
+		initTasksDB();
+        getTasks(function(task){
+            updateTasksDB(task);
+        });
+    }
+    
+    function endReloading(){
+        var tasks = getAllFromTasksDB();
+        tasks = sortTaskArray(tasks);
+        tableView.setData(tasks);
+        
+        // when you're done, just reset
+        tableView.setContentInsets({
+            top: 0
+        }, {
+            animated: true
+        });
+        reloading = false;
+        lastUpdatedLabel.text = "Last Updated: " + formatDate();
+        statusLabel.text = "Pull down to refresh...";
+        actInd.hide();
+        arrow.show();
+    }
+    
+    tableView.addEventListener('scroll', function(e){
+        var offset = e.contentOffset.y;
+        if (offset <= -65.0 && !pulling) {
+            var t = Ti.UI.create2DMatrix();
+            t = t.rotate(-180);
+            pulling = true;
+            arrow.animate({
+                transform: t,
+                duration: 180
+            });
+            statusLabel.text = "Release to refresh...";
+        }
+        else 
+            if (pulling && offset > -65.0 && offset < 0) {
+                pulling = false;
+                var t = Ti.UI.create2DMatrix();
+                arrow.animate({
+                    transform: t,
+                    duration: 180
+                });
+                statusLabel.text = "Pull down to refresh...";
+            }
+    });
+    
+    tableView.addEventListener('scrollEnd', function(e){
+        if (pulling && !reloading && e.contentOffset.y <= -65.0) {
+            reloading = true;
+            pulling = false;
+            arrow.hide();
+            actInd.show();
+            statusLabel.text = "Reloading...";
+            tableView.setContentInsets({
+                top: 60
+            }, {
+                animated: true
+            });
+            arrow.transform = Ti.UI.create2DMatrix();
+            beginReloading();
+        }
+    });
+    
+    
+    tableView.addEventListener('click', function(e){
+        Titanium.API.info("tableView event triggered: " + e.rowData.title);
         var w = createTaskEditor(e.rowData);
         
         w.open({
@@ -18,101 +178,48 @@ createNewTableView = function(tableData){
         });
     });
     
-    return tableview;
+    return tableView;
 };
 
 showTasks = function(){
-        var tasks = getAllFromTasksDB();
-		tasks = sortTaskArray(tasks);
-		
-        var tableView = createNewTableView(tasks);
-		
-		Titanium.UI.currentWindow.add(tableView);
-        
-        updateProjectView = function(tasks){
-            tableView.setData(tasks);
-        };
-        updateProjectView(tasks);
-		
-		Titanium.API.addEventListener('taskRemoved', function(_e){
-            function removeItem(element, index, array){
-                if (element.id == _e.id) {
-                    tasks.splice(index, index);
-                    Ti.API.info("Element " + index + " contains the value " + element.id + " and will be deleted with match to: " + _e.id);
-                };
-			};
-            
-            Ti.API.info("taskRemoved occured");
-            tasks.forEach(removeItem);
-            
-            updateTasksView(tasks);
-        });
-        
-        Titanium.API.addEventListener('taskItemUpdated', function(_e){
-        
-            Ti.API.info("taskItemUpdated occured");
-            
-            updateTasksView(tasks);
-        });
-    };
+    var tasks = getAllFromTasksDB();
+    tasks = sortTaskArray(tasks);
     
-    showTasks();
+    var tableView = createNewTableView();
+    
+    Titanium.UI.currentWindow.add(tableView);
+    
+    updateTasksView = function(tasks){
+        tableView.setData(tasks);
+    };
+    updateTasksView(tasks);
+    
+    Titanium.API.addEventListener('taskRemoved', function(_e){
+        function removeItem(element, index, array){
+            if (element.id == _e.id) {
+                tasks.splice(index, index);
+                Ti.API.info("Element " + index + " contains the value " + element.id + " and will be deleted with match to: " + _e.id);
+            };
+                    };
+        
+        Ti.API.info("taskRemoved occured");
+        tasks.forEach(removeItem);
+        
+        updateTasksView(tasks);
+    });
+    
+    Titanium.API.addEventListener('taskItemUpdated', function(_e){
+    
+        Ti.API.info("taskItemUpdated occured");
+        
+        updateTasksView(tasks);
+    });
+};
+
+showTasks();
 
 
-//xhr.open("POST", 'https://meldon.org/gtd/mobile.php?openid_user_id=http://openid-provider.appspot.com/' + user + '&password=' + pass + '&action=tasks');
-//
-//xhr.onload = function(){
-//    var tasks = [];
-//    try {
-//        var doc = this.responseXML.documentElement;
-//        var items = doc.getElementsByTagName("task");
-//        
-//        var x = 0;
-//        var doctitle = doc.evaluate("//result/tasks/task/name/text()").item(0).nodeValue;
-//        for (var c = 0; c < items.length; c++) {
-//            var item = items.item(c);
-//            parseTasks(tasks, item);
-//            
-//        }
-//        
-//        
-//        tasks = sortTaskArray(tasks);
-//        var tableView = createNewTableView();
-//        
-//        updateTasksView = function(data){
-//            tableView.setData(data);
-//        };
-//        
-//        updateTasksView(tasks);
-//        Titanium.UI.currentWindow.add(tableView);
-//        
-//        Titanium.API.addEventListener('taskRemoved', function(_e){
-//            function removeItem(element, index, array){
-//                if (element.id == _e.id) {
-//                    tasks.splice(index, index);
-//                    Ti.API.info("Element " + index + " contains the value " + element.id + " and will be deleted with match to: " + _e.id);
-//                };
-//			};
-//            
-//            Ti.API.info("taskRemoved occured");
-//            tasks.forEach(removeItem);
-//            
-//            updateTasksView(tasks);
-//        });
-//        
-//        Titanium.API.addEventListener('taskItemUpdated', function(_e){
-//        
-//            Ti.API.info("taskItemUpdated occured");
-//            
-//            updateTasksView(tasks);
-//        });
-//        
-//    } 
-//    catch (E) {
-//        alert(E);
-//    }
-//};
-//xhr.send();
+
 
 
 
