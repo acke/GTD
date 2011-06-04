30a9587873c01a52e5f8b0ac839596f14dfa75bf
Knut Funkel <knut.funkel@gmail.com>
2011-06-04 09:42:16 +0200

    Added refresh on project table view load (with activity indicator)

--- Resources/Views/projectsView.js
+++ Resources/Views/projectsView.js
@@ -5,11 +5,21 @@
     var projects = [];
     var user = Titanium.App.Properties.getString("user");
     var pass = Titanium.App.Properties.getString("pass");
-    
+	
+	var projWin = Titanium.UI.currentWindow;
+	
+    var actInd = Titanium.UI.createActivityIndicator({
+        bottom: 10,
+        height: 50,
+        width: 10,
+		top: 20,
+        style: Titanium.UI.iPhone.ActivityIndicatorStyle.BIG
+    });
+	
     createNewTableView = function(projects){
         var tableview = Titanium.UI.createTableView();
         tableview.setData(projects);
-		
+        
         tableview.addEventListener('click', function(e){
             Titanium.API.info("tableview event triggered: " + e.rowData.title);
             var w = createProjectEditor(e.rowData);
@@ -24,17 +34,34 @@
     
     
     showProjects = function(){
+		projWin.add(actInd);
+        actInd.show();
+        setTimeout(endReloading, 2000);
+        
+        initProjectDB();
+        
+        getProjects(function(project){
+            updateProjectsDB(project);
+            
+        });
+
+    };
+    
+    function endReloading(){
         var projects = getAllFromProjectsDB();
-		
+        
         var tableView = createNewTableView(projects);
-		
-		Titanium.UI.currentWindow.add(tableView);
+        
+        projWin.add(tableView);
         
         updateProjectView = function(projects){
             tableView.setData(projects);
         };
         updateProjectView(projects);
-    };
+		actInd.hide();
+        
+    }
+	
     
     showProjects();
     
